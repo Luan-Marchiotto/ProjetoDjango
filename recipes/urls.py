2a0d3758567d1516e4167c24375a1d6f81d836f0
Luan Marchiotto Silva <luanmarchiotto@gmail.com>
2025-03-10 20:13:38 -0300

    Config page

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from recipes.views import home
-from django.conf import settings
-from django.conf.urls.static import static
+from . import views
 
 urlpatterns = [
-    path('', home),
+    path('', views.home),
+    path('recipes/<int:id>/', views.recipe)
 ]
